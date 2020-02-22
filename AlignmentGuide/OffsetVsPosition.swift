4e6deb8a592aff0feeef65574654e38e9b5235be
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-22 09:02:03 +0530

    initial view

---
+++ AlignmentGuide/OffsetVsPosition.swift
@@ -0,0 +1,21 @@
+//
+//  OffsetVsPosition.swift
+//  AlignmentGuide
+//
+//  Created by Heshan Yodagama on 2/22/20.
+//  Copyright © 2020 Heshan Yodagama. All rights reserved.
+//
+
+import SwiftUI
+
+struct OffsetVsPosition: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OffsetVsPosition_Previews: PreviewProvider {
+    static var previews: some View {
+        OffsetVsPosition()
+    }
+}
