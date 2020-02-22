6cded87b95ece1f32a7484e9b3771c4fe1cbf6a3
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-22 09:37:25 +0530

    add position

--- AlignmentGuide/OffsetVsPosition.swift
+++ AlignmentGuide/OffsetVsPosition.swift
@@ -10,7 +10,8 @@ import SwiftUI
 
 struct OffsetVsPosition: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
+            .position(x: 100, y: 100)
     }
 }
 
